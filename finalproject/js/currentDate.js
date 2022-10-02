c009c0710b015e07b44b61f4e2f2a916bc774490
VICTOR <103970485+VictorDGauna@users.noreply.github.com>
2022-10-02 02:53:33 -0300

    Update currentDate.js

--- finalproject/js/currentDate.js
+++ finalproject/js/currentDate.js
@@ -1,6 +1,6 @@
 let date = new Date();
 let currentYear = date.getFullYear();
-document.querySelector('#currentYear').textContent = currentYear;
+
 
 let lastModified = document.lastModified;
 document.getElementById('currentDate').textContent = lastModified;
